6296cb2ee2ccfeb85399209eaf1bb8629961fbbb
tjhv10 <achiyabennatan@gmail.com>
2024-06-25 23:54:30 +0300

    good

--- tests/test_CSTV.py
+++ tests/test_CSTV.py
@@ -24,7 +24,7 @@ class TestFunctions(unittest.TestCase):
                 donor[key] = 0
         for alg_str in ["ewt", "ewtc", "mt", "mtc"]:
             self.projects = Instance([Project("A", 27),Project("B", 30),Project("C", 40)])
-            selected_projects = cstv_budgeting_combination(self.donors, self.projects, alg_str)
+            selected_projects = cstv_budgeting_combination(self.projects, self.donors, alg_str)
             self.assertEqual(len(selected_projects), 0)  # Ensure no projects are selected when budget is zero
 
     def test_cstv_budgeting_with_budget_less_than_min_project_cost(self):
@@ -34,7 +34,7 @@ class TestFunctions(unittest.TestCase):
             donor["C"] = 1
         for alg_str in ["ewt", "ewtc", "mt", "mtc"]:
             self.projects = Instance([Project("A", 27),Project("B", 30),Project("C", 40)])
-            selected_projects = cstv_budgeting_combination(self.donors, self.projects, alg_str)
+            selected_projects = cstv_budgeting_combination(self.projects, self.donors, alg_str)
             self.assertEqual(len(selected_projects), 0)  # Ensure no projects are selected when total budget is less than the minimum project cost
 
     def test_cstv_budgeting_with_budget_greater_than_max_total_needed_support(self):
@@ -45,14 +45,14 @@ class TestFunctions(unittest.TestCase):
         for alg_str in ["ewt", "ewtc", "mt", "mtc"]:
             self.projects = Instance([Project("A", 27),Project("B", 30),Project("C", 40)])
             self.donors = [CumulativeBallot({"A": 5, "B": 10, "C": 5}), CumulativeBallot({"A": 10, "B": 10, "C": 0}), CumulativeBallot({"A": 0, "B": 15, "C": 5}), CumulativeBallot({"A": 0, "B": 0, "C": 20}), CumulativeBallot({"A": 15, "B": 5, "C": 0})]
-            selected_projects = cstv_budgeting_combination(self.donors, self.projects, alg_str)
+            selected_projects = cstv_budgeting_combination(self.projects, self.donors, alg_str)
             self.assertEqual(len(selected_projects), num_projects-1)  # Ensure all projects are selected when budget exceeds the total needed support
 
     def test_cstv_budgeting_with_budget_between_min_and_max(self):
         for alg_str in ["ewt", "ewtc", "mt", "mtc"]:
             self.projects = Instance([Project("A", 27),Project("B", 30),Project("C", 40)])
             self.donors = [CumulativeBallot({"A": 5, "B": 10, "C": 5}), CumulativeBallot({"A": 10, "B": 10, "C": 0}), CumulativeBallot({"A": 0, "B": 15, "C": 5}), CumulativeBallot({"A": 0, "B": 0, "C": 20}), CumulativeBallot({"A": 15, "B": 5, "C": 0})]
-            selected_projects = cstv_budgeting_combination(self.donors, self.projects, alg_str)
+            selected_projects = cstv_budgeting_combination(self.projects, self.donors, alg_str)
             self.assertEqual(len(selected_projects), 2)  # Ensure the number of selected projects is 3 when total budget is between the minimum and maximum costs
 
     def test_cstv_budgeting_with_budget_exactly_matching_required_support(self):
@@ -62,7 +62,7 @@ class TestFunctions(unittest.TestCase):
                 donor["B"] = 30/len(self.donors)
                 donor["C"] = 40/len(self.donors)
             self.projects = Instance([Project("A", 27),Project("B", 30),Project("C", 40)])
-            selected_projects = cstv_budgeting_combination(self.donors, self.projects, alg_str)
+            selected_projects = cstv_budgeting_combination(self.projects, self.donors, alg_str)
             self.assertEqual(len(selected_projects), 3)  # Ensure all projects are selected when the total budget matches the required support exactly
 
     def test_cstv_budgeting_large_input(self):
@@ -72,7 +72,7 @@ class TestFunctions(unittest.TestCase):
             self.projects += [Project(f"Project_{i+50}", 151) for i in range(int(num_projects/2))]
             self.projects = Instance(init = self.projects)
             self.donors = [CumulativeBallot({f"Project_{i}": 1 for i in range(num_projects)}) for _ in range(100)]
-            selected_projects = cstv_budgeting_combination(self.donors, self.projects, alg_str)
+            selected_projects = cstv_budgeting_combination(self.projects, self.donors, alg_str)
             self.assertLessEqual(len(selected_projects), num_projects)  # Ensure the number of selected projects does not exceed the total number of projects
 
 
@@ -90,7 +90,7 @@ class TestFunctions(unittest.TestCase):
             num_projects = len(self.projects)
             positive_excess = sum(1 for p in self.projects if sum(donor.get(p.name, 0) for donor in self.donors) - p.cost >= 0)
             support = sum(sum(donor.values()) for donor in self.donors)
-            selected_projects = cstv_budgeting_combination(self.donors, self.projects, alg_str)
+            selected_projects = cstv_budgeting_combination(self.projects, self.donors, alg_str)
             total_cost = sum(project.cost for project in selected_projects)
             self.assertLessEqual(len(selected_projects), num_projects)  # Ensure the number of selected projects does not exceed the total number of projects
             self.assertGreaterEqual(len(selected_projects), positive_excess)  # Ensure the number of selected projects is at least the number of projects with non-negative excess support
