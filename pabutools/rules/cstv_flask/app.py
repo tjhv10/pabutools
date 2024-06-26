3a07f7af8ccedd371f8c1973a5ef2b527eaa4f4b
tjhv10 <achiyabennatan@gmail.com>
2024-06-26 13:22:55 +0300

    done

--- pabutools/rules/cstv_flask/app.py
+++ pabutools/rules/cstv_flask/app.py
@@ -1,8 +1,7 @@
-from flask import Flask, flash, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
 from cstv import *
 
 app = Flask(__name__)
-app.secret_key = 'supersecretkey'
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
@@ -17,25 +16,15 @@ def index():
         
         # Processing donors input
         donors_list = []
-        total_sum = 0
-        flag = True
         for donor_data in donations_data:
-            donor_donations = list(map(int, donor_data.split(',')))
-            donation_dict = {projects[i]: donor_donations[i] for i in range(len(donor_donations))}
-            if flag:
-                total_sum = sum(donation_dict.values())
-                flag = False
-            else:
-                if total_sum != sum(donation_dict.values()):
-                    flash("Warning: The total donation for a donor does not match the expected amount.")
+            donor_donations = map(str, donor_data.split(','))
+            donation_dict = {projects[i]: int(donation) for i, donation in enumerate(donor_donations)}
             donors_list.append(CumulativeBallot(donation_dict))
-
         donors_profile = Profile(donors_list)
         # Call the CSTV algorithm
-        selected_projects = cstv_budgeting_combination(projects_list, donors_profile, "ewt")
+        selected_projects = cstv_budgeting_combination(projects_list, donors_profile,"ewt")
 
         return render_template('result.html', selected_projects=selected_projects)
-
     return render_template('index.html')
 
 if __name__ == '__main__':
